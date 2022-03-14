2d52a08cdcb301f7f8d5a1d6e69033f6bc441140
Ömer Faruk Korkmaz <thefoxing06@gmail.com>
2022-03-14 20:59:40 +0300

    Added files

---
+++ 2022/scapy/tutorial/tutorial/spiders/quotes_spidey.py
@@ -0,0 +1,25 @@
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+
+    page_count = 0
+    book_count = 1
+
+    start_urls = [
+        "https://www.kitapyurdu.com/index.php?route=product/category&sort=purchased_365&order=DESC&filter_category_all=true&path=1&filter_in_stock=1"
+    ]
+
+    def parse(self, response, **kwargs):
+        book_names = response.css("div.name.ellipsis a::attr(title)").extract()
+        author_names = response.css("div.author.compact.ellipsis a::text").extract()
+        publisher_names = response.css("div.publisher a span::text").extract()
+        for i in zip(book_names,author_names,publisher_names):
+            yield {
+                "title" : i[0],
+                "author" : i[1],
+                "publisher" : i[2]
+            }
+        #with open("deneme.txt","a",encoding="utf-8") as file:
+        #    file.write()
