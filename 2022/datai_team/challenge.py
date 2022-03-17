c46cbb778609e269fba19815eae42c62fce5a21b
Ömer Faruk Korkmaz <thefoxing06@gmail.com>
2022-03-17 18:34:23 +0300

    Update challenge

--- 2022/datai_team/challenge.py
+++ 2022/datai_team/challenge.py
@@ -29,3 +29,55 @@ print(shuffle_letters("ankara","arxkan"))""" # shuffle letters
         letter += str(j) + j * i
     return letter
 print(shuffle_letters("ankara"))""" # letter frequently
+"""
+def kayipBasamak(str_:str):
+    str_splitted = str_.split(" ")
+    num_list = []
+    forget_data_index = 0
+    operator_ = "+"
+    if str_splitted[1] == "+": operator_ = "-"
+    elif str_splitted[1] == "-": operator_ = "-"
+    elif str_splitted[1] == "*":operator_ = "/"
+    elif str_splitted[1] == "/":operator_ = "*"
+
+    for i in str_splitted:
+        if "x" in i: forget_data_index = str_splitted.index(i)
+        try: num_list.append(int(i))
+        except: pass
+
+    num_list.sort(reverse=True)
+    data = str(int(eval(f"{num_list[0]} {operator_} {num_list[1]}")))
+
+    dicti = dict()
+    for i,j in zip(str_splitted[forget_data_index],data):
+        if i != j: dicti[i] = j
+    return dicti
+
+print(kayipBasamak("20 - 1x = 4"))
+print(kayipBasamak("1x0 / 3 = 50"))
+print(kayipBasamak("1x * 11 = 121"))
+print(kayipBasamak("36 + 1x = 50"))
+print(kayipBasamak("36 + 1xyz = 1371"))
+""" # kayıp basamak
+"""def array_rotation(liste:list):
+    string = ""
+    for i in (liste[liste[0]:] + liste[:liste[0]]):
+        string += str(i)
+    return string
+
+print(array_rotation([2,3,4,5]))
+print(array_rotation([4,5,6,7,8,9,10,11,12,13]))""" # array rotasyonu
+"""def array_pair(liste:list):
+    sliced_list = []
+    old_value = 0
+    for i in range(0,len(liste) + 1,2):
+        sliced_list.append(liste[old_value:i])
+        old_value = i
+    sliced_list.remove([])
+
+    value = "ok"
+    for i in sliced_list:
+        if sliced_list.index(i) == sliced_list.index(i[::-1]):
+            value = i
+    return value
+""" # array pair
