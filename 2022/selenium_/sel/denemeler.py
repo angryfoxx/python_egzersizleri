f556d72d8071d0c81e5456773b356fc75ab43788
Ömer Faruk Korkmaz <thefoxing06@gmail.com>
2022-04-05 14:59:56 +0300

    Files added

--- 2022/selenium_/sel/denemeler.py
+++ 2022/selenium_/sel/denemeler.py
@@ -1,2 +1,8 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+
+options = ChromeOptions()
+options.binary_location = "C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe"
+driver = webdriver.Chrome(options=options)
+
+driver.quit()
