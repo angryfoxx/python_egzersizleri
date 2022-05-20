14163c2a171ac568c91da6483932f80143ecdcd5
Ömer Faruk Korkmaz <thefoxing06@gmail.com>
2022-05-20 11:45:37 +0300

    Files Updated

--- 2022/Django/mysite/polls/views.py
+++ 2022/Django/mysite/polls/views.py
@@ -1,13 +1,28 @@
+#from django.http import Http404
 from django.http import HttpResponse
+#from django.template import loader
+from django.shortcuts import render, get_object_or_404
+
 from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    #template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list' : latest_question_list
+    }
+    #return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse(f"You are looking at question {question_id}")
+    question = get_object_or_404(Question, pk = question_id)
+    return render(request, 'polls/detail.html', {'question' : question})
+"""def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist !")
+    return render(request, 'polls/detail.html', {'question' : question})"""
 
 def results(request, question_id):
     return HttpResponse(f"You are looking at the results of the question {question_id}")
